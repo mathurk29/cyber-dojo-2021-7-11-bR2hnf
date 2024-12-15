720fba26deca48e2a96acc4d3f482a258f7b478b
MygzHC <MygzHC@cyber-dojo.org>
2024-12-16 00:06:58 +0530

    pytest skip demo test

--- files/test_hiker.py
+++ files/test_hiker.py
@@ -1,7 +1,12 @@
+import pytest
 from hiker import global_answer, Hiker
 
+
+@pytest.mark.skip
 def test_global_function():
     assert global_answer() == 42
 
+
+@pytest.mark.skip
 def test_instance_method():
     assert Hiker().instance_answer() == 42
